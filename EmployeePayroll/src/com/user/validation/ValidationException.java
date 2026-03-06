8c53b0c8369d3f3d3b8300512090bf55bcc80af6
anerudh21 <anerudh21@gmail.com>
2026-03-06 08:11:23 +0530

    UC-1 Implemented user registration functionality to employee payroll app

---
+++ EmployeePayroll/src/com/user/validation/ValidationException.java
@@ -0,0 +1,9 @@
+package com.user.validation;
+
+@SuppressWarnings("serial")
+public class ValidationException extends Exception {
+	public ValidationException(String  message) {
+		super(message);
+	}
+
+}
